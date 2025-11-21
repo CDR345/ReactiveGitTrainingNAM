d4e5fd9e5f4ea236d1f4d5cefbb4ead89eb096e3
Vijaya <v.vijaya1@gmail.com>
2025-11-21 11:37:32 +0000

    add

---
+++ profile-demo/src/test/java/com/neueda/profile_demo/ProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.profile_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
